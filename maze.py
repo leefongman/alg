d5392921ca7c67c26607a18f75ef612eebf2bad0
leefong <foolishboys@foxmail.com>
2017-11-11 13:30:59 +0800

    迷宫求解算法添加注释

--- maze.py
+++ maze.py
@@ -2,11 +2,12 @@
 
 import time
 import os
-
+#  移动方向列表
 DIRECTION = [[0, 1], [1, 0], [-1, 0], [0, -1]]
 
 
 def getMap():
+    #  迷宫创建
     m = [
         "#####################",
         "#     ##          #S#",
@@ -24,10 +25,16 @@ def getMap():
 
 
 def showMap(mazeMap):
+    """
+    迷宫求解状态显示
+    """
+    #  将光标移动到第一行第一列
     print("\033[1;1H", end='', flush=True)
+    #  状态显示过程
     for row in mazeMap:
         for ch in row:
             if ch == '@':
+                #  红色高亮字符
                 print("\033[31m" + ch + "\033[0m", end='', flush=True)
             else:
                 print(ch, end='')
@@ -35,6 +42,9 @@ def showMap(mazeMap):
 
 
 def getPos(mazeMap, ch):
+    """
+    返回特定字符所在的行列坐标
+    """
     for row, rowCh in enumerate(mazeMap):
         for col, char in enumerate(rowCh):
             if char == ch:
@@ -42,20 +52,32 @@ def getPos(mazeMap, ch):
 
 
 def init(mazeMap, startCh, stopCh):
+    """
+    返回开始与结束字符的行列坐标
+    """
+    #  清屏
     print("\033[2J", end='', flush=True)
+
     return getPos(mazeMap, startCh), getPos(mazeMap, stopCh)
 
 
 def maze(mazeMap, start, stop):
+    """
+    迷宫求解过程
+    """
     showMap(mazeMap)
     time.sleep(0.1)
 
     for direct in DIRECTION:
+        #  循环四个方向求解迷宫
         new = [start[0] + direct[0], start[1] + direct[1]]
         if mazeMap[new[0]][new[1]] == ' ':
+            #  更新迷宫状态
             mazeMap[new[0]][new[1]] = '@'
+            #  显示最新迷宫状态
             maze(mazeMap, new, stop)
         elif new == stop:
+            #  迷宫成功求解,退出程序
             os._exit(0)
 
     mazeMap[start[0]][start[1]] = ' '
@@ -64,6 +86,9 @@ def maze(mazeMap, start, stop):
 
 
 def main():
+    """
+    主程序
+    """
     mazeMap = getMap()
     start, stop = init(mazeMap, 'S', 'E')
     maze(mazeMap, start, stop)
