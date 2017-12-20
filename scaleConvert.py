a7883e011eec262e54c79227ae23668133837223
leefong <foolishboys@foxmail.com>
2017-12-20 20:19:22 +0800

    x进制转y进制

--- scaleConvert.py
+++ scaleConvert.py
@@ -1,48 +1,46 @@
 #!/usr/bin/env python3
 
-def tenToCh(num):
+
+def ten2Ch(num):
     """
     数字转字符
     """
-    if num < 10:
-        return chr(num + 48)
-    elif num >= 10:
-        return chr(97 + num - 10)
+    return chr(num + 48) if num < 10 else chr(num - 10 + 97)
 
 
-def tenToX(num, x):
+def ten2X(num, x):
     """
     十进制转x进制
     """
     if num < x:
-        return tenToCh(num)
+        return ten2Ch(num)
 
-    return tenToX(num // x, x) + tenToCh(num % x)
+    return ten2X(num // x, x) + ten2Ch(num % x)
 
 
 def ten(s):
     """
     字符转数字
     """
-    if s > "9":
-        return ord(s) - 97 + 10
-
-    return int(s)
+    return (ord(s) - 97 + 10) if s > "9" else (ord(s) - 48)
 
 
-def xToTen(numStr, x):
+def x2Ten(numStr, x):
     """
     x进制转十进制
     """
     if len(numStr) == 1:
         return ten(numStr)
 
-    return xToTen(numStr[:-1]) * x + ten(numStr[-1])
+    return x2Ten(numStr[:-1], x) * x + ten(numStr[-1])
 
 
-def xToY(x, numStr, y):
+def x2y(x, numStr, y):
     """
     x进制转y进制
     """
-    return tenToX(xToTen(numStr, x), y)
+    return ten2X(x2Ten(numStr, x), y)
 
+
+if __name__ == "__main__":
+    print(x2y(10, '234689723468716', 16))
